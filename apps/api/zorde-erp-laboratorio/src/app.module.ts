171d97148ff8bd85185465300c8d02d95d8662a2
luis_guilherme <gui_castro_xd@hotmail.com>
2026-06-29 18:06:45 -0300

    feat(auth): integrate JwtAuthGuardStrategy as a global guard in the application module for enhanced security

--- apps/api/zorde-erp-laboratorio/src/app.module.ts
+++ apps/api/zorde-erp-laboratorio/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import databaseConfig from './config/database.config';
 import { validateEnv } from './config/env.validation';
 import { DatabaseModule } from './infra/database/database.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { JwtAuthGuardStrategy } from './modules/auth/presentation/guards/jwtAuth.guard';
 import { ClienteModule } from './modules/cliente/cliente.module';
 import { FornecedorModule } from './modules/fornecedor/fornecedor.module';
 import { OrdemDeServicoModule } from './modules/ordemDeServico/ordemDeServico.module';
@@ -30,6 +32,12 @@ import { UsuarioModule } from './modules/usuario/usuario.module';
 		OrdemDeServicoModule,
 	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [
+		AppService,
+		{
+			provide: APP_GUARD,
+			useClass: JwtAuthGuardStrategy,
+		},
+	],
 })
 export class AppModule {}
