c2fd9c3fda3a76732e1dd73966fc71cd83b4c4c6
luis_guilherme <gui_castro_xd@hotmail.com>
2026-07-18 00:34:50 -0300

    fix(osFolhaPdf): correct browser connection check in getBrowser method

--- apps/api/zorde-erp-laboratorio/src/modules/cliente/infrastructure/services/osFolhaPdf.service.ts
+++ apps/api/zorde-erp-laboratorio/src/modules/cliente/infrastructure/services/osFolhaPdf.service.ts
@@ -23,7 +23,7 @@ export class OsFolhaPdfService implements OnModuleDestroy {
 	}
 
 	private async getBrowser(): Promise<Browser> {
-		if (!this.browser?.connectedS) {
+		if (!this.browser?.connected) {
 			this.browser = await puppeteer.launch({
 				headless: true,
 				args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage'],
